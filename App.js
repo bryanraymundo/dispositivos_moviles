1de3fe6a0a1540caea68d42d52f4f7051a884f72
Bryan Emanuel Hernandez Raymundo <androbryaan@gmail.com>
2022-05-10 00:11:56 -0500

    Cmt2

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
+import react from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/Components/Login/Login';
-import Casita from './src/Components/Home/Home';
-import react from 'react';
+import Login from './src/Screens/Login/Login';
+import Home from './src/Screens/Home/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login/>
-      <Casita/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>{/* ... */}</Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    color: '#fff',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00ff00',
     alignItems: 'center',
     justifyContent: 'center',
   },
